69b16388a8f67aed8539ab63004304b06c91a163
Max Linke <max_linke@gmx.de>
2017-05-23 23:28:53 +0200

    add reading of coordinates

--- pytng/pytng.pyx
+++ pytng/pytng.pyx
@@ -1,4 +1,9 @@
 from libc.stdint cimport int64_t
+from libc.stdlib cimport malloc, free
+
+import numpy as np
+cimport numpy as np
+np.import_array()
 
 ctypedef enum tng_function_status: TNG_SUCCESS, TNG_FAILURE, TNG_CRITICAL
 
@@ -19,12 +24,31 @@ cdef extern from "tng/tng_io.h":
         const tng_trajectory_t tng_data,
         int64_t *n)
 
+    tng_function_status tng_num_particles_get(
+        const tng_trajectory_t tng_data,
+        int64_t *n)
+
+    tng_function_status tng_distance_unit_exponential_get(
+        const tng_trajectory_t tng_data,
+        int64_t *exp);
+
+    tng_function_status tng_util_pos_read_range(
+        const tng_trajectory_t tng_data,
+        const int64_t first_frame,
+        const int64_t last_frame,
+        float **positions,
+        int64_t *stride_length)
+
+
 cdef class TNGFile:
     cdef tng_trajectory_t _traj
     cdef readonly fname
     cdef str mode
     cdef int is_open
-    cdef readonly int64_t _n_frames
+    cdef int64_t _n_frames
+    cdef int64_t _n_atoms
+    cdef int64_t pos
+    cdef float distance_scale
 
     def __cinit__(self, fname, mode='r'):
         self.fname = fname
@@ -46,16 +70,28 @@ cdef class TNGFile:
             raise IOError('mode must be one of "r" or "w", you '
                           'supplied {}'.format(mode))
 
-        cdef int ok
+        cdef int64_t exponent, ok
+
         ok = tng_util_trajectory_open(fname, _mode, & self._traj)
         if ok != TNG_SUCCESS:
             raise IOError("An error ocurred opening the file. {}".format(status_error_message[ok]))
 
-        ok = tng_num_frames_get(self._traj, &self._n_frames)
-        if ok != TNG_SUCCESS:
-            raise IOError("An error ocurred reading n_frames. {}".format(status_error_message[ok]))
+        if self.mode == 'r':
+            ok = tng_num_frames_get(self._traj, &self._n_frames)
+            if ok != TNG_SUCCESS:
+                raise IOError("An error ocurred reading n_frames. {}".format(status_error_message[ok]))
+
+            ok = tng_num_particles_get(self._traj, & self._n_atoms)
+            if ok != TNG_SUCCESS:
+                raise IOError("An error ocurred reading n_atoms. {}".format(status_error_message[ok]))
+
+            ok = tng_distance_unit_exponential_get(self._traj, &exponent)
+            if ok != TNG_SUCCESS:
+                raise IOError("An error ocurred reading distance unit exponent. {}".format(status_error_message[ok]))
+            self.distance_scale = 10.0**(exponent+9)
 
         self.is_open = True
+        self.pos = 0
 
     def close(self):
         if self.is_open:
@@ -79,5 +115,33 @@ cdef class TNGFile:
             raise IOError('No file currently opened')
         return self._n_frames
 
+    @property
+    def n_atoms(self):
+        if not self.is_open:
+            raise IOError('No file currently opened')
+        return self._n_atoms
+
     def __len__(self):
         return self.n_frames
+
+    def read(self):
+        if self.pos >= self._n_frames:
+            raise EOFError
+
+        cdef np.ndarray[ndim=2, dtype=np.float32_t, mode='c'] xyz = np.empty((self.n_atoms, 3), dtype=np.float32)
+        cdef float* positions = NULL
+        cdef int64_t stride_length, ok, i
+
+        ok = tng_util_pos_read_range(self._traj, self.pos, self.pos, &positions, &stride_length)
+        if ok != TNG_SUCCESS:
+            if positions != NULL:
+                free(positions)
+            raise IOError("error reading frame")
+
+        for i in range(self._n_atoms):
+            for j in range(3):
+                xyz[i, j] = positions[i*3 + j]
+        xyz *= self.distance_scale
+
+        self.pos += 1
+        return xyz
--- tests/test_tng.py
+++ tests/test_tng.py
@@ -9,16 +9,20 @@ def test_len(TNG_REF_DATA, TNG_REF_FILEPATH):
         assert TNG_REF_DATA.length == tng.n_frames
 
 
-def test_natoms(TNG_REF_DATA):
-    assert TNG_REF_DATA.natoms == 15
+def test_natoms(TNG_REF_DATA, TNG_REF_FILEPATH):
+    with pytng.TNGFile(TNG_REF_FILEPATH) as tng:
+        assert TNG_REF_DATA.natoms == tng.n_atoms
 
 
-def test_first_positions(TNG_REF_DATA):
-    assert np.array_equal(TNG_REF_DATA.first_frame, TNG_REF_DATA.first_frame)
+def test_first_positions(TNG_REF_DATA, TNG_REF_FILEPATH):
+    with pytng.TNGFile(TNG_REF_FILEPATH) as tng:
+        first_frame = tng.read()
+        assert np.array_equal(TNG_REF_DATA.first_frame, first_frame)
 
 
 def test_last_positions(TNG_REF_DATA):
     assert TNG_REF_DATA.last_frame[0, 0] == 1.01562
+
 
 def test_path(TNG_REF_FILEPATH):
     assert isinstance(TNG_REF_FILEPATH, str)
