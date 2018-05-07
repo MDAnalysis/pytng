8f1ae79431a78e069b2c369861d473c75918c2d9
Oliver Beckstein <orbeckst@gmail.com>
2018-05-07 16:45:31 -0700

    create sitemap.xml
    - install https://github.com/jdillard/sphinx-sitemap
    - see https://github.com/MDAnalysis/MDAnalysis.github.io/issues/78

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,10 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_sitemap']
+
+# for sitemap with https://github.com/jdillard/sphinx-sitemap
+site_url = "https://www.mdanalysis.org/pytng/"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
