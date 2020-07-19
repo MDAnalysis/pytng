ba6cfa4764bd0726ba5f13201f6da4d2b0459ed8
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2020-07-19 14:41:04 +1000

    change tests

--- tests/test_tng.py
+++ tests/test_tng.py
@@ -43,200 +43,195 @@ def test_iter(TNG_EXAMPLE):
             assert i == ts.step
 
 
-# @pytest.mark.parametrize('slice_idx', [
-#     (None, None, None),
-#     (2, None, None),
-#     (None, 2, None),
-#     (None, None, 3),
-#     (1, 3, None),
-#     (None, None, -1),
-#     (0, -1, None),
-#     (None, 99, None),  # Out of bound
-# ])
-# def test_sliced_iteration(slice_idx, TNG_EXAMPLE):
-#     start, stop, step = slice_idx
-#     ref_steps = np.arange(0, 10)[start:stop:step]
+@pytest.mark.parametrize('slice_idx', [
+    (None, None, None),
+    (2, None, None),
+    (None, 2, None),
+    (None, None, 3),
+    (1, 3, None),
+    (None, None, -1),
+    (0, -1, None),
+    (None, 99, None),  # Out of bound
+])
+def test_sliced_iteration(slice_idx, TNG_EXAMPLE):
+    start, stop, step = slice_idx
+    ref_steps = np.arange(0, 10)[start:stop:step]
 
-#     with pytng.TNGFile(TNG_EXAMPLE) as tng:
-#         for ref_ts, ts in zip(ref_steps, tng[start:stop:step]):
-#             assert ref_ts == ts.step
-
-
-# @pytest.mark.parametrize('slx', (
-#     [0, 1, 2],
-#     [5, 3, 1],
-#     [1, 1, 1],
-#     [0, -1, 0],
-#     [-2, -3, -4],
-# ))
-# @pytest.mark.parametrize('cls', [list, np.array])
-# def test_getitem_multipl_ints(slx, cls, GMX_REF_DATA, GMX_REF_FILEPATH):
-#     slx = cls(slx)
-#     indices = np.arange(GMX_REF_DATA.length)
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         for ref_step, ts in zip(indices[slx], tng[slx]):
-#             assert ref_step == ts.step
+    with pytng.TNGFile(TNG_EXAMPLE) as tng:
+        for ref_ts, ts in zip(ref_steps, tng[start:stop:step]):
+            assert ref_ts == ts.step
 
 
-# @pytest.mark.parametrize('idx', [0, 4, 9, -1, -2])
-# def test_getitem_int(idx, GMX_REF_DATA, GMX_REF_FILEPATH):
-#     indices = np.arange(GMX_REF_DATA.length)
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         ts = tng[idx]
-#         assert ts.step == indices[idx]
+@pytest.mark.parametrize('slx', (
+    [0, 1, 2],
+    [5, 3, 1],
+    [1, 1, 1],
+    [0, -1, 0],
+    [-2, -3, -4],
+))
+@pytest.mark.parametrize('cls', [list, np.array])
+def test_getitem_multipl_ints(slx, cls, TNG_EXAMPLE):
+    slx = cls(slx)
+    indices = np.arange(TNG_EXAMPLE.length)
+    with pytng.TNGFile(TNG_EXAMPLE) as tng:
+        for ref_step, ts in zip(indices[slx], tng[slx]):
+            assert ref_step == ts.step
 
 
-# @pytest.mark.parametrize('idx', [
-#     'a',
-#     'invalid',
-#     (0, 1),
-#     lambda x: x
-# ])
-# def test_getitem_single_invalid(idx, GMX_REF_FILEPATH):
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         with pytest.raises(TypeError) as excinfo:
-#             tng[idx]
-#     message = ("Trajectories must be an indexed using an integer,"
-#                " slice or list of indices")
-#     assert message in str(excinfo.value)
+@pytest.mark.parametrize('idx', [0, 4, 9, -1, -2])
+def test_getitem_int(idx, TNG_EXAMPLE):
+    indices = np.arange(TNG_EXAMPLE.length)
+    with pytng.TNGFile(TNG_EXAMPLE) as tng:
+        ts = tng[idx]
+        assert ts.step == indices[idx]
 
 
-# @pytest.mark.parametrize('arr', (
-#     [T] * 10,
-#     [F] * 10,
-#     [T, F, T, F, T, F, T, F, T, F]
-# ))
-# @pytest.mark.parametrize('cls', [list, np.array])
-# def test_getitem_bool(arr, cls, GMX_REF_DATA, GMX_REF_FILEPATH):
-#     slx = cls(arr)
-#     ref = np.arange(GMX_REF_DATA.length)[slx]
-
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         for ref_ts, ts in zip(ref, tng[slx]):
-#             assert ref_ts == ts.step
-
-
-# @pytest.mark.parametrize('cls', [list, np.array])
-# def test_getitem_bool_TypeError(cls, GMX_REF_FILEPATH):
-#     slx = cls([True, False, True])
-
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         with pytest.raises(TypeError):
-#             for ts in tng[slx]:
-#                 ts.step
+@pytest.mark.parametrize('idx', [
+    'a',
+    'invalid',
+    (0, 1),
+    lambda x: x
+])
+def test_getitem_single_invalid(idx, TNG_EXAMPLE):
+    with pytng.TNGFile(TNG_EXAMPLE) as tng:
+        with pytest.raises(TypeError) as excinfo:
+            tng[idx]
+    message = ("Trajectories must be an indexed using an integer,"
+               " slice or list of indices")
+    assert message in str(excinfo.value)
 
 
-# def test_natoms(GMX_REF_DATA, GMX_REF_FILEPATH):
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         assert GMX_REF_DATA.natoms == tng.n_atoms
+@pytest.mark.parametrize('arr', (
+    [T] * 10,
+    [F] * 10,
+    [T, F, T, F, T, F, T, F, T, F]
+))
+@pytest.mark.parametrize('cls', [list, np.array])
+def test_getitem_bool(arr, cls, GMX_REF_DATA, GMX_REF_FILEPATH):
+    slx = cls(arr)
+    ref = np.arange(TNG_EXAMPLE.length)[slx]
+
+    with pytng.TNGFile(TNG_EXAMPLE) as tng:
+        for ref_ts, ts in zip(ref, tng[slx]):
+            assert ref_ts == ts.step
 
 
-# def test_first_positions(GMX_REF_DATA, GMX_REF_FILEPATH):
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         first_frame = tng.read().positions
-#         assert np.array_equal(GMX_REF_DATA.first_frame, first_frame)
+@pytest.mark.parametrize('cls', [list, np.array])
+def test_getitem_bool_TypeError(cls, TNG_EXAMPLE):
+    slx = cls([True, False, True])
+    with pytng.TNGFile(TNG_EXAMPLE) as tng:
+        with pytest.raises(TypeError):
+            for ts in tng[slx]:
+                ts.step
 
 
-# def test_last_positions(GMX_REF_DATA, GMX_REF_FILEPATH):
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         tng.seek(tng.n_frames - 1)
-#         last_frame = tng.read().positions
-#         assert np.array_equal(GMX_REF_DATA.last_frame, last_frame)
+def test_natoms(GMX_REF_DATA, TNG_EXAMPLE):
+    with pytng.TNGFile(TNG_EXAMPLE) as tng:
+        assert GMX_REF_DATA.natoms == tng.n_atoms
 
 
-# @pytest.mark.parametrize('idx', [-11, -12, 10, 11])
-# def test_seek_IndexError(idx, GMX_REF_FILEPATH):
-#     with pytng.TNGFile(GMX_REF_FILEPATH, 'r') as tng:
-#         with pytest.raises(IndexError):
-#             tng[idx]
+def test_first_positions(GMX_REF_DATA, TNG_EXAMPLE):
+    with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
+        first_frame = tng.read().positions
+        assert np.array_equal(GMX_REF_DATA.first_frame, first_frame)
 
 
-# @pytest.mark.skip(reason="Write mode not implemented yet.")
-# def test_seek_write(MISSING_FILEPATH):
-#     with pytng.TNGFile(MISSING_FILEPATH, mode='w') as tng:
-#         with pytest.raises(IOError) as excinfo:
-#             tng.seek(0)
-#         assert "seek not allowed in write mode" in str(excinfo.value)
+def test_last_positions(GMX_REF_DATA, TNG_EXAMPLE):
+    with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
+        tng.seek(tng.n_frames - 1)
+        last_frame = tng.read().positions
+        assert np.array_equal(GMX_REF_DATA.last_frame, last_frame)
 
 
-# def test_seek_not_open(GMX_REF_FILEPATH):
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         pass
-#     with pytest.raises(IOError) as excinfo:
-#         tng.seek(0)
-#     assert 'No file currently opened' in str(excinfo.value)
+@pytest.mark.parametrize('idx', [-11, -12, 10, 11])
+def test_seek_IndexError(idx, TNG_EXAMPLE):
+    with pytng.TNGFile(GMX_REF_FILEPATH, 'r') as tng:
+        with pytest.raises(IndexError):
+            tng[idx]
 
 
-# def test_time(GMX_REF_DATA, GMX_REF_FILEPATH):
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         for ref_time, ts in zip(GMX_REF_DATA.time, tng):
-#             assert ref_time == ts.time
+@pytest.mark.skip(reason="Write mode not implemented yet.")
+def test_seek_write(MISSING_FILEPATH):
+    with pytng.TNGFile(MISSING_FILEPATH, mode='w') as tng:
+        with pytest.raises(IOError) as excinfo:
+            tng.seek(0)
+        assert "seek not allowed in write mode" in str(excinfo.value)
 
 
-# def test_box(GMX_REF_DATA, GMX_REF_FILEPATH):
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         frame = tng.read()
-#         assert np.array_equal(GMX_REF_DATA.box, frame.box)
+def test_seek_not_open(TNG_EXAMPLE):
+    with pytng.TNGFile(TNG_EXAMPLE) as tng:
+        pass
+    with pytest.raises(IOError) as excinfo:
+        tng.seek(0)
+    assert 'No file currently opened' in str(excinfo.value)
 
 
-# def test_double_iteration(GMX_REF_FILEPATH):
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         for i, frame in enumerate(tng):
-#             assert i == frame.step
-
-#         for i, frame in enumerate(tng):
-#             assert i == frame.step
+def test_time(GMX_REF_DATA, TNG_EXAMPLE):
+    with pytng.TNGFile(TNG_EXAMPLE) as tng:
+        for ref_time, ts in zip(GMX_REF_DATA.time, tng):
+            assert ref_time == ts.time
 
 
-# @pytest.mark.parametrize('prop', ('n_frames', 'n_atoms'))
-# def test_property_not_open(prop, GMX_REF_FILEPATH):
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         pass
-#     with pytest.raises(IOError) as excinfo:
-#         getattr(tng, prop)
-#     assert 'No file currently opened' in str(excinfo.value)
 
 
-# def test_tell(GMX_REF_FILEPATH):
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         for step, frame in enumerate(tng, start=1):
-#             assert step == tng.tell()
+def test_double_iteration(TNG_EXAMPLE):
+    with pytng.TNGFile(TNG_EXAMPLE) as tng:
+        for i, frame in enumerate(tng):
+            assert i == frame.step
+
+        for i, frame in enumerate(tng):
+            assert i == frame.step
 
 
-# def test_read_not_open(GMX_REF_FILEPATH):
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         pass
-#     with pytest.raises(IOError) as excinfo:
-#         tng.read()
-#     assert 'No file opened' in str(excinfo.value)
+@pytest.mark.parametrize('prop', ('n_frames', 'n_atoms'))
+def test_property_not_open(prop, GMX_REF_FILEPATH):
+    with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
+        pass
+    with pytest.raises(IOError) as excinfo:
+        getattr(tng, prop)
+    assert 'No file currently opened' in str(excinfo.value)
 
 
-# @pytest.mark.skip(reason="Write mode not implemented yet.")
-# def test_read_not_mode_r(MISSING_FILEPATH):
-#     with pytest.raises(IOError) as excinfo:
-#         with pytng.TNGFile(MISSING_FILEPATH, mode='w') as tng:
-#             tng.read()
-#     assert 'Reading only allow in mode "r"' in str(excinfo.value)
+def test_tell(TNG_EXAMPLE):
+    with pytng.TNGFile(TNG_EXAMPLE) as tng:
+        for step, frame in enumerate(tng, start=1):
+            assert step == tng.tell()
 
 
-# def test_seek_reset_eof(GMX_REF_FILEPATH):
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         for ts in tng:
-#             pass
-#         tng.seek(0)
-#         next(tng)
+def test_read_not_open(TNG_EXAMPLE):
+    with pytng.TNGFile(TNG_EXAMPLE) as tng:
+        pass
+    with pytest.raises(IOError) as excinfo:
+        tng.read()
+    assert 'No file opened' in str(excinfo.value)
 
 
-# def test_reached_eof(GMX_REF_FILEPATH):
-#     with pytng.TNGFile(GMX_REF_FILEPATH) as tng:
-#         # test with iter protocol
-#         for ts in tng:
-#             pass
-#         with pytest.raises(StopIteration):
-#             next(tng)
-#         # test __getitem__
-#         tng.seek(0)
-#         for ts in tng[:]:
-#             pass
-#         with pytest.raises(StopIteration):
-#             next(tng)
+@pytest.mark.skip(reason="Write mode not implemented yet.")
+def test_read_not_mode_r(MISSING_FILEPATH):
+    with pytest.raises(IOError) as excinfo:
+        with pytng.TNGFile(MISSING_FILEPATH, mode='w') as tng:
+            tng.read()
+    assert 'Reading only allow in mode "r"' in str(excinfo.value)
+
+
+def test_seek_reset_eof(TNG_EXAMPLE):
+    with pytng.TNGFile(TNG_EXAMPLE) as tng:
+        for ts in tng:
+            pass
+        tng.seek(0)
+        next(tng)
+
+
+def test_reached_eof(TNG_EXAMPLE):
+    with pytng.TNGFile(TNG_EXAMPLE) as tng:
+        # test with iter protocol
+        for ts in tng:
+            pass
+        with pytest.raises(StopIteration):
+            next(tng)
+        # test __getitem__
+        tng.seek(0)
+        for ts in tng[:]:
+            pass
+        with pytest.raises(StopIteration):
+            next(tng)
