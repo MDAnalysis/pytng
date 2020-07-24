28381fb11e1d303a02bb3761c9c4cc3d187f3f13
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2020-07-24 20:49:19 +1000

    pep8

--- tests/test_tng_block_iterator.py
+++ tests/test_tng_block_iterator.py
@@ -1,15 +1,19 @@
 import pytng
 import numpy as np
 from numpy.testing import (
-    assert_almost_equal, assert_equal, assert_array_almost_equal)
+    assert_almost_equal,
+    assert_equal,
+    assert_array_almost_equal,
+)
 import pytest
 
 T, F = True, False
 
+
 def test_file_open(TNG_EXAMPLE):
-    file_iterator = pytng.TNGFileIterator(TNG_EXAMPLE, mode='r')
+    file_iterator = pytng.TNGFileIterator(TNG_EXAMPLE, mode="r")
+
 
 def test_next_block_call(TNG_EXAMPLE):
-    file_iterator = pytng.TNGFileIterator(TNG_EXAMPLE, mode='r')
+    file_iterator = pytng.TNGFileIterator(TNG_EXAMPLE, mode="r")
     file_iterator.read_next_block()
-    raise Exception
