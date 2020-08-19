d22a8283f84550267f334ae7fe9e68cd98dcb17e
hugo <hugomacdermott@gmail.com>
2020-08-19 11:11:41 +1000

    pep8 testscript

--- tests/testscript.py
+++ tests/testscript.py
@@ -2,17 +2,17 @@ import pytng
 import numpy as np
 
 
-positions = np.zeros((1000,3), dtype=np.float32)
-box = np.zeros((1,9), dtype=np.float32)
-lmbda = np.zeros((1,1), dtype=np.float32)
+positions = np.zeros((1000, 3), dtype=np.float32)
+box = np.zeros((1, 9), dtype=np.float32)
+lmbda = np.zeros((1, 1), dtype=np.float32)
 
 with pytng.TNGFileIterator("./reference_files/argon_npt_compressed.tng", mode="r", debug=False) as tng:
     print(tng.block_ids)
-    for i in range(0,500000,5000):
+    for i in range(0, 500000, 5000):
         tng.read_step(i)
-        tng.current_integrator_step.get_blockid(268435457, positions)
-        tng.current_integrator_step.get_blockid(1152921504875282432,lmbda)
-        tng.current_integrator_step.get_blockid(268435456, box)
+        tng.current_integrator_step.get_positions(positions)
+        tng.current_integrator_step.get_blockid(1152921504875282432, lmbda)
+        tng.current_integrator_step.get_box(box)
         print(positions)
         print(lmbda)
         print(box)
