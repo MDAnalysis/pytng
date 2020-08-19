fee00514d77abd1849efb7ebfd3d53f123a6fcd6
hugo <hugomacdermott@gmail.com>
2020-08-19 11:17:17 +1000

    change testscript

--- tests/testscript.py
+++ tests/testscript.py
@@ -8,12 +8,13 @@ lmbda = np.zeros((1, 1), dtype=np.float32)
 
 with pytng.TNGFileIterator("./reference_files/argon_npt_compressed.tng", mode="r", debug=False) as tng:
     print(tng.block_ids)
-    for i in range(0, 500000, 5000):
+    for i in range(0, len(tng), 5000):
         tng.read_step(i)
-        tng.current_integrator_step.get_positions(positions)
+        tng.current_integrator_step.get_pos(positions)
         tng.current_integrator_step.get_blockid(1152921504875282432, lmbda)
         tng.current_integrator_step.get_box(box)
+        time = tng.current_integrator_step.get_time()
         print(positions)
         print(lmbda)
         print(box)
-        print(tng.current_integrator_step.time)
+        print(time)
