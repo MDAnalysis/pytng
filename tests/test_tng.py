42c5d6601871815dd8d450a04cf859010b2a4aca
hugo <hugomacdermott@gmail.com>
2020-10-13 16:24:27 +1100

    add test for UTF8 special char

--- tests/test_tng.py
+++ tests/test_tng.py
@@ -15,6 +15,10 @@ def test_tng_example_load_bad_file(CORRUPT_FILEPATH):
         with pytng.TNGFileIterator(CORRUPT_FILEPATH) as tng:
             tng.read_step(0)
 
+def test_tng_example_load_utf8_special_char(TNG_UTF8_EXAMPLE):
+    with pytng.TNGFileIterator(TNG_UTF8_EXAMPLE) as tng:
+        tng.read_step(0)
+
 
 def test_tng_example_open_missing_file_mode_r(MISSING_FILEPATH):
     with pytest.raises(IOError) as excinfo:
