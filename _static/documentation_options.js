ab965281beca2f8111ed9106c4a7d8e182a62896
Hugo MacDermott-Opeskin (via Travis CI) <hugomacdermott@gmail.com>
2020-12-04 10:55:57 +0000

    Deploy pytng to github.com/MDAnalysis/pytng.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.2.2+1.gbd7220e',
+    VERSION: 'v0.2.2+2.g1cdc65c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
