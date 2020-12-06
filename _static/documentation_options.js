c68b2f66d87d1dd3925d795c5761b044faa00278
hmacdope <hugomacdermott@gmail.com>
2020-12-06 22:52:42 +0000

    Deploying to gh-pages from  @ 0f232127e41a61e01ea75b83988f68ea24ddbde1 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g831ef76',
+    VERSION: '0+untagged.1.g0f23212',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
