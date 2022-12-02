9c34c6ca633be1dccfc7e9b933ce0f6d69b29636
IAlibay <IAlibay@users.noreply.github.com>
2022-12-02 23:32:30 +0000

    Deploying to gh-pages from  @ e87245cc5281ed183cdd3d1f47f7205a9552103c 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gb99462d',
+    VERSION: '0+untagged.1.ge87245c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
